73920634e0e4d615c32bef4cfe61c1da6a658377
vicente550 <vicente.lagos.vl@gmail.com>
2020-11-05 17:13:55 -0300

    creando formularios

--- InfoGames/catalogo/urls.py
+++ InfoGames/catalogo/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('videojuego/<int:pk>', views.VideoJuegoDetailView.as_view(), name='videojuego-detail'),
 
+]
+
+urlpatterns+= [
+    path('videojuego/create/', views.VideoJuegoCreate.as_view(), name='videojuego-create'),
+    path('videojuego/<int:pk>/update/', views.VideoJuegoUpdate.as_view(), name='videojuego-update'),
+    path('videojuego/<int:pk>/delete/', views.VideoJuegoDelete.as_view(), name='videojuego-delete'),
 ]
--- InfoGames/catalogo/views.py
+++ InfoGames/catalogo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import VideoJuego
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 def index(request):
     num_VideoJuego= VideoJuego.objects.all().count()
@@ -9,3 +12,20 @@ def index(request):
         'index.html',
         context={'num_VideoJuego': num_VideoJuego},
     )
+
+class VideoJuegoCreate(CreateView):
+    model = VideoJuego
+    fields = '__all__'
+    initial ={'departure': '19/11/2020'}
+
+class VideoJuegoUpdate(UpdateView):
+    model = VideoJuego
+    fields = ['id', 'name', 'departure', 'price']
+
+class VideoJuegoDelete(DeleteView):
+    model = VideoJuego
+    success_url = reverse_lazy('videojuego')
+
+class VideoJuegoDetailView(generic.DetailView):
+    model = VideoJuego
+
